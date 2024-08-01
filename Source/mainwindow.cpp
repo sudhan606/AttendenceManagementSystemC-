d756c51699c1e5186504478583e2be1374dff97b
Sudhan Bhattarai <sudhanbhattarai606@gmail.com>
2024-08-01 16:48:09 +0545

    changes

--- Source/mainwindow.cpp
+++ Source/mainwindow.cpp
@@ -23,16 +23,14 @@ imageLabel1(new QLabel(this))
    headingLabel->setStyleSheet("font-size: 25px; font-weight:600;color:#FFFFFF; background-color:transparent;");
    headingLabel2->setGeometry(50, 184, 270, 32);
    headingLabel2->setStyleSheet("font-size: 13px; font-weight: 200; color:#E6E2E2;background-color:transparent;");
-   //ALL STYLE IS IN STYLE.cpp file*/
+   //ALL STYLE IS IN STYLE.cpp file
     attendanceEntryButton->setGeometry(50, 250, 97, 35);
      attendanceEntryButton->setStyleSheet(attbtn);
    adminLoginButton->setGeometry(161, 250, 97, 35);
      adminLoginButton->setStyleSheet(loginbtn);
 
    QPixmap pixmap("C:/Users/HP/Documents/att/pic2.png");  // Load the c logo
-   int newWidthHeigt = 50; // Desired width
-   //int newHeight = 40; // Desired height
-   //QPixmap scaledPixmap = pixmap.scaled(newWidth, newHeight, Qt::KeepAspectRatio);  // Scale the pixmap
+   int newWidthHeigt = 50; 
   imageLabel->setPixmap(pixmap);  // Set the scaled pixmap on the label
    imageLabel->setGeometry(55, 19,  newWidthHeigt,  newWidthHeigt); // Position and size the label
 
@@ -42,7 +40,7 @@ imageLabel1(new QLabel(this))
     connect(timer, &QTimer::timeout, this, &MainWindow::updateDateTime);
     timer->start(1000); // Update every second
     updateDateTime();
-    // Set the size of the main window
+    // fixedsize of the main window
     setFixedSize(700, 500); // Width: 800, Height: 600
     setWindowTitle("ATTENDER");
 }
@@ -55,9 +53,9 @@ void MainWindow::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
 
     // Set the pen and brush for the painter
-    QPen pen(QColor("#C79EF0"), 0.0000001); // Black pen with width 2
+    QPen pen(QColor("#C79EF0"), 0.0000001); 
     painter.setPen(pen);
-    QBrush brush(QColor("#C79EF0")); // Blue brush
+    QBrush brush(QColor("#C79EF0")); 
     painter.setBrush(brush);
 
     // Draw a circle
